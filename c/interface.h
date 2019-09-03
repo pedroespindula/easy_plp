78bbd746bf08b55c525b315f2aa806b03b67e345
Pedro Espindula <joaopedro.jpse@gmail.com>
2019-09-02 21:56:08 -0300

    Remove argumento nome de funções

--- c/interface.h
+++ c/interface.h
@@ -4,9 +4,9 @@ char* get_msg_welcome(void);
 char* get_msg_help(void);
 char* get_msg_exit(void);
 
-char* get_fmsg_exercise_info(char* exercise_id, char* exercise_name);
+char* get_fmsg_exercise_info(char* exercise_id);
 char* get_fmsg_exercise_results(char* file_path, char* language, char* test_results);
-char* get_fmsg_download_exercise(char* exercise_id, char* exercise_name);
+char* get_fmsg_download_exercise(char* exercise_id);
 
 char* get_prompt_select_option(void);
 char* get_prompt_download_exercise(void);
