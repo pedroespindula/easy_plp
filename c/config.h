6a27843a0387943b35c57a2c7fbb3b9f25a699df
Pedro Espindula <joaopedro.jpse@gmail.com>
2019-09-02 22:10:10 -0300

    Adiciona header files de configuração

---
+++ c/config.h
@@ -0,0 +1,6 @@
+char* get_config_exercises_path(void);
+char* set_config_exercises_path(char* new_path);
+char* get_config_username(void);
+char* set_config_username(char* new_name);
+char* get_config_registry(void);
+char* set_config_registry(char* new_registry);
