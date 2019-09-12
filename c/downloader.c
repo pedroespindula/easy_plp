ea4873794892311c92e6cb31deed8a935d8929e0
Anderson Clemente <anderson.filipe.silva@ccc.ufcg.edu.br>
2019-09-12 06:03:40 -0300

    Cria  funcionalidade para download pelo id

--- c/downloader.c
+++ c/downloader.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
+#include <string.h>
 #include <curl/curl.h>
 
 char* dl_exercise(char* exercise_id, char* dest_dir){
+	
+	char* urlExercicios = "https://raw.githubusercontent.com/pedroespindula/easy_plp/master/exercicios/";
+	char* urlCompleta;
 
-}
+	urlCompleta = malloc(strlen(urlExercicios)+strlen(exercise_id)+1);
+	strcpy(urlCompleta, urlExercicios);
+	strcat(urlCompleta, exercise_id);
+
+	CURL *curl;
+	FILE *fp;
+    CURLcode res;
+	
+	curl = curl_easy_init();
+	if (curl) {
+       	fp = fopen(dest_dir,"wb");
+       	curl_easy_setopt(curl, CURLOPT_URL, urlCompleta);
+       	curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+       	res = curl_easy_perform(curl);
+       	curl_easy_cleanup(curl);
+       	fclose(fp);
+ 	}
+};
 
 int main(){
-
-
+	dl_exercise("dobro.csv","../exercicios/teste1.csv");
 	return 0;
 }
+
