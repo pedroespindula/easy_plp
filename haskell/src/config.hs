f37e55fe7ab71f3e01200445fdaa0805264a6ed9
Anderson Clemente <anderson.filipe.silva@ccc.ufcg.edu.br>
2019-10-18 07:58:35 -0300

    Termina config

--- haskell/src/config.hs
+++ haskell/src/config.hs
@@ -3,29 +3,17 @@ import System.IO
 config :: String -> String -> String -> IO ()
 config name matricula diretorio = writeFile "config.txt" (name++"\n"++matricula++"\n"++diretorio)
 
-my_read :: IO String
-my_read = readFile "girlfriend.txt"
+my_read :: Int -> IO (String)
+my_read x = do
+	contents <- readFile "config.txt"
+	let text = (lines contents) !! x
+	return text
 
-run :: IO (Int, Int, [(Int,Int,Int)])
-run = do
-  contents <- readFile "text.txt"   -- use '<-' here so that 'contents' is a String
-  let [a,b,c] = lines contents      -- split on newlines
-  let firstLine  = read (init a)    -- 'init' drops the trailing period
-  let secondLine = read (init b)    
-  let thirdLine  = read (init c)    -- this reads a list of Int-tuples
-  return (firstLine, secondLine, thirdLine)
---get_line_x = do 
---	let [name, matricula, diretorio] = lines (my_read) 
---	name
---get_config_name :: [String]
---get_config_name = get_tine_x 1
+get_config_name :: IO String
+get_config_name = my_read 0
 
---get_config_matricula :: String
---get_config_matricula = "o"
+get_config_matricula :: IO String
+get_config_matricula = my_read 1
 
---get_config_diretorio_exercicios :: String
---get_config_diretorio_exercicios = "m"
---
-
-
-
+get_config_diretorio_exercicios :: IO String
+get_config_diretorio_exercicios = my_read 2
