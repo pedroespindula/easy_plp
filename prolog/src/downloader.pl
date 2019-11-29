f9b625a7dd0ca473d49258ad02aa07373cde481f
Arthur Guedes <arthur.guedes@ccc.ufcg.edu.br>
2019-11-29 14:29:12 -0300

    Finaliza

--- prolog/src/downloader.pl
+++ prolog/src/downloader.pl
@@ -1,4 +1,5 @@
 :- use_module(library(http/http_open)).
+:-initialization (main).
 
 downloader(Exercise) :-
     URL = "https://raw.githubusercontent.com/pedroespindula/easy_plp/init-prolog/exercicios/",
@@ -12,9 +13,20 @@ downloader(Exercise) :-
 
 lerCsvRowList(Exercise, Lists) :-
     csv_read_file(Exercise, Rows, []),
-    rows_to_lists(Rows, Lists).
+    rowsToLists(Rows, Lists).
 
-rows_to_lists(Rows, Lists) :- maplist(row_to_list, Rows, Lists).
+getByIndex([],Index,[]).
+getByIndex([H|T],Index, [H1|T1]):-
+    nth0(Index,H,H1),
+    getByIndex(T,Index, T1).
 
-row_to_list(Row, List) :-
+rowsToLists(Rows, Lists) :- maplist(rowToList, Rows, Lists).
+
+rowToList(Row, List) :-
     Row =.. [row|List].
+
+main:-
+    downloader("dobro"),
+    lerCsvRowList("./dobro.csv", [_|Lists]),
+    getByIndex(Lists,1,R),  
+    write(R).
