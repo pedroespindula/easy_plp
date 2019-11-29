eaa78043d941a24416926bfc93a053548286246a
Arthur Guedes <arthur.guedes@ccc.ufcg.edu.br>
2019-11-29 13:28:52 -0300

    Adiciona funcionalide criar arquivo csv

--- prolog/src/downloader.pl
+++ prolog/src/downloader.pl
@@ -1,12 +1,11 @@
 :- use_module(library(http/http_open)).
 
-downloader(Exercise, []) :-
+downloader(Exercise) :-
     URL = "https://raw.githubusercontent.com/pedroespindula/easy_plp/init-prolog/exercicios/",
     string_concat(URL, Exercise, PartialURL),
     string_concat(PartialURL, ".csv", CompleteURL),
+    string_concat(Exercise, ".csv", File),
     http_open(CompleteURL, In, []),
-    copy_stream_data(In, user_output),
+    open(File, write, Stream),
+    copy_stream_data(In, Stream),
     close(In).
-
-:- initialization
-    downloader("dobro", []).
